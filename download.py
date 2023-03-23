d21e888095711f9aef4fa76abaec941d92d5d7fa
f-pochat <federico.pochat@ing.austral.edu.ar>
2023-03-23 14:41:41 -0300

    First

---
+++ download.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# Download the 56 zip files in Images_png in batches
+import urllib.request
+import sys
+import io
+
+# URLs for the zip files
+links = [
+    # 'https://nihcc.box.com/shared/static/vfk49d74nhbxq3nqjg0900w5nvkorp5c.gz',
+    'https://nihcc.box.com/shared/static/i28rlmbvmfjbl8p2n3ril0pptcmcu9d1.gz',
+    'https://nihcc.box.com/shared/static/f1t00wrtdk94satdfb9olcolqx20z2jp.gz',
+	'https://nihcc.box.com/shared/static/0aowwzs5lhjrceb3qp67ahp0rd1l1etg.gz',
+    'https://nihcc.box.com/shared/static/v5e3goj22zr6h8tzualxfsqlqaygfbsn.gz',
+	'https://nihcc.box.com/shared/static/asi7ikud9jwnkrnkj99jnpfkjdes7l6l.gz',
+	'https://nihcc.box.com/shared/static/jn1b4mw4n6lnh74ovmcjb8y48h8xj07n.gz',
+    'https://nihcc.box.com/shared/static/tvpxmn7qyrgl0w8wfh9kqfjskv6nmm1j.gz',
+	'https://nihcc.box.com/shared/static/upyy3ml7qdumlgk2rfcvlb9k6gvqq2pj.gz',
+	'https://nihcc.box.com/shared/static/l6nilvfa9cg3s28tqv1qc1olm3gnz54p.gz',
+	'https://nihcc.box.com/shared/static/hhq8fkdgvcari67vfhs7ppg2w6ni4jze.gz',
+	'https://nihcc.box.com/shared/static/ioqwiy20ihqwyr8pf4c24eazhh281pbu.gz'
+]
+
+for idx, link in enumerate(links):
+    with urllib.request.urlopen(link) as Response:
+        Length = Response.getheader('content-length')
+        BlockSize = 1000000  # default value
+
+        if Length:
+            Length = int(Length)
+            BlockSize = max(4096, Length // 20)
+
+        print("UrlLib len, blocksize: ", Length, BlockSize)
+
+        BufferAll = io.BytesIO()
+        Size = 0
+        while True:
+            BufferNow = Response.read(BlockSize)
+            if not BufferNow:
+                break
+            BufferAll.write(BufferNow)
+            Size += len(BufferNow)
+            if Length:
+                Percent = int((Size / Length)*100)
+                print(f"download: {Percent}% {idx}")
+
+        print("Buffer All len:", len(BufferAll.getvalue()))
+
+print("Download complete. Please check the checksums")
